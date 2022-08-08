5dafd8c29feb7cc79f488c4bfb023c8c9887181e
dskim <ektha115@gmail.com>
2022-08-08 15:55:02 +0900

    7.5 React Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import { useState, useEffect } from "react";
-import Movie from "./components/Movie";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+/* URL을 보고있는 Component */
 function App() {
-  return null;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail></Detail>
+        </Route>
+        {/* 유저가 이 경로에 있으면 Home Route를 렌더링해준다. */}
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
