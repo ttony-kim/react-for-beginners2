bf343bc3ec9508b37f2cb3ff0aa00142498531ee
dskim <ektha115@gmail.com>
2022-08-08 10:53:43 +0900

    7.0 To Do List part One

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
 import { useState, useEffect } from "react";
-function Hello() {
-  // Hello 컴포넌트가 그려지지 않을 때는 'destroy' 됐기 때문에 아래 코드가 실행 되지 않는다.
-  // Hello 컴포넌트가 그려질때는 '생성'되는 것이기 때문에 아래 코드가 실행 된다.
-  useEffect(() => {
-    console.log("CREATED :)");
-    // cleanup function: component가 destroy 될때 실행시켜주는 함수
-    return () => console.log("DESTROYED :(");
-  }, []);
-
-  // function byFn() {
-  //   console.log("bye :(");
-  // }
-  // function hiFn() {
-  //   console.log("created :)");
-  //   return byFn;
-  // }
-  // useEffect(hiFn, []);
-  return <h1>Hello</h1>;
-}
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => {
+    console.log(event.target.value);
+    setToDo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => {
+      return [toDo, ...currentArray];
+    });
+    setToDo("");
+  };
+
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
