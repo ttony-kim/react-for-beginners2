0dbc8edfd1f5245d912f26ecc154e9e1c6fbde7c
DASOM <ektha115@gmail.com>
2022-08-09 23:14:58 +0900

    Hook: useAxios

---
+++ src/hooks/useAxios/index.js
@@ -0,0 +1,17 @@
+import ReactDOM from "react-dom";
+import useAxios from "./useAxios";
+
+const App = () => {
+  const { loading, data, error, refetch } = useAxios({
+    url: "https://yts.mx/api/v2/list_movies.json",
+  });
+  return (
+    <div>
+      <h1>{data && data.status}</h1>
+      <h2>{loading && "Loading"}</h2>
+      <button onClick={refetch}>Refetch</button>
+    </div>
+  );
+};
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
