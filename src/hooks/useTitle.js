ca300d6a6637522838b9f23d27fdf3f74ce38b9b
dskim <ektha115@gmail.com>
2022-08-09 13:19:46 +0900

    Hook: useTitle

---
+++ src/hooks/useTitle.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+export const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
